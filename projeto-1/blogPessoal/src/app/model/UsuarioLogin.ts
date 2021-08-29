293ae964b7c674fd4152b6f807aafbd83c982235
kembg <kennedymoises2@gmail.com>
2021-08-28 22:28:02 -0300

    implementação de rotas

---
+++ projeto-1/blogPessoal/src/app/model/UsuarioLogin.ts
@@ -0,0 +1,10 @@
+export class UsuarioLogin{
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public token: string
+    public dataNascimento: string
+    public foto: string
+    public tipo: string
+}
