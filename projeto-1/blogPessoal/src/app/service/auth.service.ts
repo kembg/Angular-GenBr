8ed8614c2cfeff29af3a3b2c22562d08bfa73be8
kembg <kennedymoises2@gmail.com>
2021-09-16 19:46:19 -0300

    get de postagens tbm

--- projeto-1/blogPessoal/src/app/service/auth.service.ts
+++ projeto-1/blogPessoal/src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,12 +14,20 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
   entrar(usuarioLogin : UsuarioLogin): Observable<UsuarioLogin> {
     return this.http.post<UsuarioLogin>('https://blogpessoalkennedy.herokuapp.com/usuarios/logar', usuarioLogin)
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogpessoalkennedy.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  getByIdUsuario(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://blogpessoalkennedy.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
